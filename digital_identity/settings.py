972ee35db4cf399db30000dca13ece02db18f3f1
nielsdaw <nielsdaw@gmail.com>
2016-11-20 15:59:36 +0100

    changed settings for deployment

--- digital_identity/settings.py
+++ digital_identity/settings.py
@@ -173,10 +173,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 
