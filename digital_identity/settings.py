d5564d8d4f4fd0562b846c25049aee3787f70ada
nielsdaw <nielsdaw@gmail.com>
2016-11-20 20:50:07 +0100

    changed staticfiles to static when debug= False

--- digital_identity/settings.py
+++ digital_identity/settings.py
@@ -177,10 +177,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 
