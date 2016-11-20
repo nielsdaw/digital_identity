2fe8333292fb000f5368db1200d8a2fd8b3c0c6f
nielsdaw <nielsdaw@gmail.com>
2016-11-20 20:51:33 +0100

    changed staticfiles to static when debug= False

--- digital_identity/settings.py
+++ digital_identity/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
