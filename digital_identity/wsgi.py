6d5a9903f48cf36eab69e8ccbf6c2a0b06ceafba
nielsdaw <nielsdaw@gmail.com>
2016-11-21 12:16:22 +0100

    module in wsgi file updated

--- digital_identity/wsgi.py
+++ digital_identity/wsgi.py
@@ -13,7 +13,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_me.digital_identity.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digital_identity.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
