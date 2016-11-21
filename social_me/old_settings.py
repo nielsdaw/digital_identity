4f0a383d8077fa26e2923effcebb754a26318737
nielsdaw <nielsdaw@gmail.com>
2016-11-21 18:03:14 +0100

    added logging to see error 500

--- social_me/old_settings.py
+++ social_me/old_settings.py
@@ -17,7 +17,7 @@ from .config import *
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
