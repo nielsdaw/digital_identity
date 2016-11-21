b35bbc730fa688992c53414d3f80582b05f25696
nielsdaw <nielsdaw@gmail.com>
2016-11-21 13:46:02 +0100

    changed prod static file settings

--- social_me/settings/production.py
+++ social_me/settings/production.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
